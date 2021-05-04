0174145f14a3d6f7ba72ce2f821c4171a5bbf354
ivan heinemeier <ivan.heinemeier@gmail.com>
2021-05-04 15:11:05 +0200

    corrected grammar in lab 01

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,5 +1,5 @@
-print("hello...")
-print("""why would i use \"?
-why? Again?""")
-print("this is tab: \t??")
-print("this can also make linefeed\n??")
+print("Hello...")
+print("""Why would i use \"?
+Why? Again?""")
+print("This is tab: \t??")
+print("This can also make linefeed\n??")
