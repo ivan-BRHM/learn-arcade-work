d58df2d46437d933de22495d5266202d80b7da76
ivan heinemeier <ivan.heinemeier@gmail.com>
2021-10-04 09:00:29 +0200

    latest version of permutations_calculator.py - past_permutations not working

--- Testing/permutations_calculator.py
+++ Testing/permutations_calculator.py
@@ -9,23 +9,20 @@ for i in range(10):
     is_num_added = False
 
     print(this_permutation, "this_permutation")
+    print(past_permutations, "past permutations")
 
     for val in [num for num in seq if num not in this_permutation]:
+
         print(this_permutation + [val], "compare", this_permutation + [val] in past_permutations)
-        if not this_permutation + [val] in past_permutations:
+
+        if this_permutation + [val] not in past_permutations:
             this_permutation.append(val)
             is_num_added = True
             break
 
     if not is_num_added:
         print(this_permutation, "not popped")
-        past_permutations.append(this_permutation)
-        print(past_permutations, "past permutations")
+        past_permutations.append([this_permutation])
+        print(past_permutations)
         this_permutation.pop(-1)
         print(this_permutation, "popped")
-
-    """if len(this_permutation) == len(seq):
-        pass
-        past_permutations.append(this_permutation)
-        print(past_permutations, "past_permutations")
-"""
